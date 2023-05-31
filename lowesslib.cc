e735fcff6198f02262c69250b768eca31df492e6
Arnaldo Leon <amleon@alum.mit.edu>
2023-05-31 13:39:35 +0200

    cleanup

--- lowesslib.cc
+++ lowesslib.cc
@@ -6,6 +6,13 @@ extern "C" {
     float solve_intercept(const float *x, const float *y, float x0, float h, int n);
 }
 
+void verify(bool cond, const char*msg)
+{
+    if (cond == false) {
+        throw std::invalid_argument(msg);
+    }
+}
+
 py::array_t<float> smooth(
     py::array_t<float, py::array::f_style | py::array::forcecast> xi,
     py::array_t<float, py::array::f_style | py::array::forcecast> x,
@@ -13,21 +20,11 @@ py::array_t<float> smooth(
     float h
 )
 {
-    if (x.shape(0) != y.shape(0)) {
-        throw std::invalid_argument("x and y are not of the same length");
-    }
-    if (x.shape(0) == 0) {
-        throw std::invalid_argument("array of sample points is empty");
-    }
-    if (xi.ndim() > 1) {
-        throw std::invalid_argument("xi must be 1-dimensional");
-    }
-    if (x.ndim() > 1) {
-        throw std::invalid_argument("x must be 1-dimensional");
-    }
-    if (y.ndim() > 1) {
-        throw std::invalid_argument("y must be 1-dimensional");
-    }
+    verify(x.shape(0) == y.shape(0), "x and y are not of the same length");
+    verify(x.shape(0) > 0, "x is empty");
+    verify(xi.ndim() == 1, "xi must be 1-dimensional");
+    verify(x.ndim()  == 1, "x must be 1-dimensional");
+    verify(y.ndim()  == 1, "y must be 1-dimensional");
 
     auto yi = py::array_t<float>(xi.shape(0));
     auto x_buffer  = x.request();
@@ -41,8 +38,8 @@ py::array_t<float> smooth(
     float *py  = static_cast<float*>(y_buffer.ptr);
     int m = xi.shape(0);
     int n = x.shape(0);
-    bool ok = true; // needed to exit OMP block
 
+    bool ok = true; // needed to exit OMP block
     #pragma omp parallel for schedule(static)
     for (int i = 0; i < m; i++) {
         ok &= (PyErr_CheckSignals() == 0);
@@ -59,9 +56,6 @@ py::array_t<float> smooth(
 
 PYBIND11_MODULE(lowesslib, m)
 {
-    py::options options;
-    options.disable_function_signatures();
-
     m.doc() = "LOWESS: Locally Weighted Scatterplot Smoothing";
     m.def("smooth", &smooth, "Lowess smoothing",
           py::arg("xi"), py::arg("x"), py::arg("y"), py::arg("h"));
