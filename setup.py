ebe0d82c48397067d99520be99b5d994ce9df12e
Maksym Vorobiov <mvorobiov@volitiondev-1.co.radix-trading.com>
2025-03-20 06:29:31 -0500

    Add check for max python version, starting python 3.12 we get core dump

--- setup.py
+++ setup.py
@@ -1,5 +1,16 @@
 from distutils.core import setup, Extension
 import pybind11
+import sys
+
+MAX_PYTHON_VERSION = (3, 11)
+
+# Get the current Python version
+current_version = sys.version_info[:2]  # (major, minor)
+
+# There are API conflicts with python 3.12+ due to OpenMP api changes
+if current_version > MAX_PYTHON_VERSION:
+    raise SystemExit(f"Error: Python {sys.version} is too new. Maximum supported version is {MAX_PYTHON_VERSION[0]}.{MAX_PYTHON_VERSION[1]}.")
+
 
 module = Extension(
     "lowesslib",
