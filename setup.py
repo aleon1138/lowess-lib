f3535a0d5e470727b350a6ede4aeb64b0f3a4f71
Arnaldo Leon <amleon@alum.mit.edu>
2026-03-24 13:35:43 -0500

    Modify install scripts so that uv works

--- setup.py
+++ setup.py
@@ -1,28 +1,14 @@
-from setuptools import setup, Extension
-import pybind11
+from pybind11.setup_helpers import Pybind11Extension
+from setuptools import setup
 
-module = Extension(
-    "lowesslib",
-    extra_compile_args=["-O3", "-fopenmp", "-march=native", "-std=c++17"],
-    extra_link_args=["-fopenmp"],
-    include_dirs=[pybind11.get_include()],
-    sources=["lowess.cc", "lowesslib.cc", "expectile.cc"],
-)
+ext_modules = [
+    Pybind11Extension(
+        "lowesslib",
+        sources=["lowess.cc", "lowesslib.cc", "expectile.cc"],
+        extra_compile_args=["-O3", "-fopenmp", "-march=native"],
+        extra_link_args=["-fopenmp"],
+        cxx_std=17,
+    ),
+]
 
-setup(
-    name="lowesslib",
-    version="0.2.2",
-    author="Arnaldo Leon",
-    author_email="amleon@alum.mit.edu",
-    description="Utilities for Locally Weighted Scatterplot Smoothing",
-    long_description=open("README.md").read(),
-    long_description_context_type="text/markdown",
-    ext_modules=[module],
-    python_requires=">=3.7",
-    install_requires=[
-        "numpy>=1.18",
-    ],
-    project_urls={
-        "Source": "https://github.com/aldon1138/lowess-lib",
-    },
-)
+setup(ext_modules=ext_modules)
